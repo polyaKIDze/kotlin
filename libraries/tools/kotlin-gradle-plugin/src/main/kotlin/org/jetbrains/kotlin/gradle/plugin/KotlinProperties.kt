a8ff90a7716932baf94b0419f9684eb0d554019a
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2020-08-19 14:11:18 +0300

    Overflowed teamcity messages should failed build

--- libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinProperties.kt
+++ libraries/tools/kotlin-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinProperties.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.gradle.plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.dsl.NativeCacheKind
+import org.jetbrains.kotlin.gradle.internal.testing.TCServiceMessageOutputStreamHandler.Companion.IGNORE_TCSM_OVERFLOW
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType.Companion.jsCompilerProperty
 import org.jetbrains.kotlin.gradle.targets.native.DisabledNativeTargetsReporter
 import org.jetbrains.kotlin.gradle.tasks.AbstractKotlinCompile
@@ -183,6 +184,12 @@ internal class PropertiesProvider private constructor(private val project: Proje
      */
     val nativeCacheKind: NativeCacheKind
         get() = property("kotlin.native.cacheKind")?.let { NativeCacheKind.byCompilerArgument(it) } ?: CacheBuilder.DEFAULT_CACHE_KIND
+
+    /**
+     * Ignore overflow in [org.jetbrains.kotlin.gradle.internal.testing.TCServiceMessageOutputStreamHandler]
+     */
+    val ignoreTcsmOverflow: Boolean
+        get() = booleanProperty(IGNORE_TCSM_OVERFLOW) ?: false
 
     /**
      * Generate kotlin/js external declarations from all .d.ts files found in npm modules
